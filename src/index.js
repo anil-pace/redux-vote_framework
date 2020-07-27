b4ddfda4e6332ffd548f2379acd135daba234001
Anil kumar <anil.k@greyorange.com>
2020-07-28 02:05:12 +0530

    vote your framework

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+import myReducer from "./reducers";
+import Results from './components/results';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let store = createStore(myReducer);
+function render() {
+  ReactDOM.render(
+    <div className="container">
+      <App store={store} />
+      <hr />
+      <Results store={store} />
+    </div>,
+    document.getElementById('root')
+  );
+}
+store.subscribe(render);
+render();
